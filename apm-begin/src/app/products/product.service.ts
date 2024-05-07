c096eb1273d208f03fbd5c9a70fc04e63cc0a85e
Eric Belongea <belongeae@infinity-software.com>
2024-05-07 10:22:26 -0600

    Higher order mapping operators

--- apm-begin/src/app/products/product.service.ts
+++ apm-begin/src/app/products/product.service.ts
@@ -1,7 +1,11 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
-import { Observable, tap } from 'rxjs';
+import { Observable, catchError, map, of, switchMap, tap, throwError } from 'rxjs';
 import { Product } from './product';
+import { ProductData } from './product-data';
+import { HttpErrorService } from '../utilities/http-error.service';
+import { ReviewService } from '../reviews/review.service';
+import { Review } from '../reviews/review';
 
 @Injectable({
   providedIn: 'root'
@@ -10,21 +14,42 @@ export class ProductService {
   private productsUrl = 'api/products';
   private http = inject(HttpClient); // this does the same thing as using a constructor. 
   // constructor(private http: HttpClient) {}
+  private errorService = inject(HttpErrorService);
+  private reviewService = inject(ReviewService);
 
 
   getProducts(): Observable<Product[]> {
     return this.http.get<Product[]>(this.productsUrl)
       .pipe(
-        tap(() => console.log('In Http.get pipeline'))
+        tap(() => console.log('In Http.get pipeline')),
+        catchError(err => this.handleError(err))
       );
   }
 
   getProduct(id: number) {
     const productUrl = this.productsUrl + '/' + id;
-
     return this.http.get<Product>(productUrl)
       .pipe(
-        tap(() => console.log('In a specific product pipeline'))
+        tap(() => console.log('In a specific product pipeline')),
+        switchMap(product => this.getProductWithReviews(product)),
+        catchError(err => this.handleError(err))
       );
+  }
+
+  private getProductWithReviews(product: Product): Observable<Product> {
+    if (product.hasReviews) {
+      return this.http.get<Review[]>(this.reviewService.getReviewUrl(product.id))
+        .pipe(
+          map(reviews => ({ ...product, reviews} as Product))
+        )
+    } else {
+      return of(product)
+    }
+  }
+
+  private handleError(err: HttpErrorResponse): Observable<never> {
+    const formattedMessage = this.errorService.formatError(err);
+    return throwError(() => formattedMessage); // throwError is an Angular way of 'throw'(JS) which does the same thing as far as I can tell. Create an obserable to throw an error.
+    // throw formattedMessage;
   }
 }
